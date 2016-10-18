2338c511a9caa0a531c50f918ea2822be89ddb5d
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2016-10-18 15:29:18 -0400

    Finished Abstract database insertion classes.

--- src/Maude/AbstractMaudeLaiskInserter.php
+++ src/Maude/AbstractMaudeLaiskInserter.php
@@ -1,20 +1,33 @@
 <?php
 namespace Maude;
 
+/*
+ * Work further on design. This started as a Template Method pattern.
+ */
+
 class AbstractMaudeLasikInserter extends AbstractTableInserter {
 
   private $pdo;
+  private \PDOStatement $stmt;
 
-  public function __construct(\PDO $pdo_in) 
+  protected function bindParameters(\PDOStatement $stmt) : void;   // not implemented
+
+  protected function assignParameters(\Ds\Vector $vec) : void;     // not implemented
+
+  public function __construct(\PDO $pdo_in, string $insert_str) 
   {
        $this->pdo = $pdo_in;
 
-       $this->pdo->setExceptions(      );
+       $this->pdo->setExceptions(???);
+
+       $this->stmt = $this->pdo->prepare($insert_str); 
+
+       // template method pattern
+       bindParameters($this->stmt);
   }
 
   public function next() : void 
   {
-
   }
    
   public function valid() : bool
@@ -24,15 +37,18 @@ class AbstractMaudeLasikInserter extends AbstractTableInserter {
 
   public function rewind() : bool
   {
-
   } 
   
   public function current() : \PDOStatement;
   
   public function key() : int;
 
-
-  public function insert(\Ds\Vector $vec) : void;
-
+   // template method pattern
+  public function insert(\Ds\Vector $vec) : void
+  {
+       assignParameters($vec);       
+    
+       $thi->stmt->execute();
+  }
 }
 ?>
--- src/Maude/DeviceTableInserter.php
+++ src/Maude/DeviceTableInserter.php
@@ -4,7 +4,7 @@ namespace Maude;
 class DeviceTableInserter implements AbstractMaudeLasikIterator, \Iterator {
 
   private $pdo;
-  private $insert_stmt;
+
   private $mdr_report_key;
   private $device_product_code;
 
@@ -12,28 +12,24 @@ class DeviceTableInserter implements AbstractMaudeLasikIterator, \Iterator {
 
   public function __construct(\PDO $pdo_in) 
   {
-      parent::__construct($pdo_in); // <-- needed? 
-
-       $this->pdo = $pdo_in;
-
-       $this->pdo->setExceptions(      );
-
-      // SQL statement with named placeholders 
-      $this->insert_stmt = $this->pdo->prepare("INSERT INTO foi_device(mdr_report_key, device_product_code) values
-	      (:mdr_report_key, :device_product_code )");
-
-      // bind the parameters in each statement
-      $this->insert_stmt->bindParam(':mdr_report_key', $this->mdr_report_key, \PDO::PARAM_INT);
-        
-      $this->insert_stmt->bindParam(':device_product_code', $this->device_product_code, \PDO::PARAM_STR);
-
+      parent::__construct($pdo_in, "INSERT INTO foi_device(mdr_report_key, device_product_code) values
+	      (:mdr_report_key, :device_product_code )"); 
   }
 
-  public function next() : void 
+  protected function bindParameters(\PDOStatement $insert_stmt)
   {
-
+      // bind the parameters in each statement
+      $insert_stmt->bindParam(':mdr_report_key', $this->mdr_report_key, \PDO::PARAM_INT);
+        
+      $insert_stmt->bindParam(':device_product_code', $this->device_product_code, \PDO::PARAM_STR);
   }
-   
+
+  protected function assignParameters(\Ds\Vector $vec) : void
+  {
+    $this->mdr_report_key = $vec[0];
+    $this->device_report_code = $vec[1];
+  }
+
   public function valid() : bool
   {
      return true;
@@ -47,17 +43,5 @@ class DeviceTableInserter implements AbstractMaudeLasikIterator, \Iterator {
   public function current() : \PDOStatement;
   
   public function key() : int;
-
-
-  public function insert(\Ds\Vector $vec) : void
-  {
-       $this->mdr_report_key = $vec[0];
-       $this->device_product_code = $vec[1];
-
-       $rc = $this->insert_stmt->execute();
-  }
-
-
-  public function insert(\Ds\Vector $vec) : void;
 
 }
