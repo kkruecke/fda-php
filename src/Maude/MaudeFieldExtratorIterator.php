4f635a236dcb3e9dd6964769c3e472d5787b7a72
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2016-10-23 09:38:05 -0400

    See todo.txt for latest design changes and what need to be worked on next.

--- src/Maude/MaudeFieldExtratorIterator.php
+++ src/Maude/MaudeFieldExtratorIterator.php
@@ -1,15 +1,14 @@
 <?php
 namespace Maude;
 
-class MaudeFieldExtratorIterator extends \Iterator {
+class MaudeFieldExtratorIterator extends \RegexIterator {
 
     $this->vector; 
     $this->indecies;
-    $this->regexIter;
 
     public function __construct(\Maude\SplFileObjectExtended $spl_file_object_ex, array $indices)
     {
-        $this->regexIter = new RegexIterator($spl_file_object_ex,  '/([^|]*)\||\1$/', RegexIterator::ALL_MATCHES);
+        parent::__construct($spl_file_object_ex,  '/([^|]*)\||\1$/', RegexIterator::ALL_MATCHES);
 
         $this->vector = new \Ds\Vector;  
 
@@ -31,7 +30,4 @@ class MaudeFieldExtratorIterator extends \Iterator {
 
       return $this->vector;
     }
-
-    // TODO: Forward other member methods to $this->regexIter
 }
-
